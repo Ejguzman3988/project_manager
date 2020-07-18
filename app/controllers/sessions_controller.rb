8e13f89116a0db4577f8fd1af92778050f3b3748
ejguzman3988 <eriberto176@gmail.com>
2020-07-17 23:59:41 -0400

    Added logic to sessions controller #post '/signup'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,23 @@ class SessionsController < ApplicationController
     get '/login' do
         erb :'/sessions/login'
     end
+
+    post '/signup' do
+        if session[:failure_message]
+            session[:failure_message]
+            session[:failure_message] = nil
+        end
+
+        user = User.new(params[:user])
+
+        if user.save
+            redirect "/users/#{user.id}"
+        else
+            # Change to flash
+            session[:failure_message] = "Enter valid sign up form."
+
+            redirect '/signup'
+
+        end
+    end
 end
