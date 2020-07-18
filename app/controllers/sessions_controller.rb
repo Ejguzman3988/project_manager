eb4fa62fee395f5c81b7b94cf95f63e0872efc34
ejguzman3988 <eriberto176@gmail.com>
2020-07-18 00:50:24 -0400

    Added functionality to Login, added logout form to nav bar in layout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,12 @@ class SessionsController < ApplicationController
         erb :'/sessions/login'
     end
 
+    get '/logout' do
+        session.clear
+
+        redirect '/'
+    end
+
     post '/signup' do
         
         user = User.new(params[:user])
@@ -16,13 +22,30 @@ class SessionsController < ApplicationController
             if session[:failure_message]
                 session[:failure_message] = nil
             end
+            session[:user_id] = user.id
             redirect "/users/#{user.id}"
         else
-            # Change to flash
+            # TODO: Change to flash
             session[:failure_message] = "Enter valid sign up form."
 
             redirect '/signup'
 
         end
+    end
+
+    post '/login' do
+        session[:failure_message] = nil
+
+        user = User.find_by_username(params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect '/users'
+        else
+            # TODO: Change to flash
+            session[:failure_message] = "Enter valid login in form."
+
+            redirect '/login'
+        end
+        
     end
 end
