b5e649c71d9d2e92b9da40b045ead588dfa54ee5
ejguzman3988 <eriberto176@gmail.com>
2020-07-21 04:32:31 -0400

    reset database and seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-user.create(name: "Robo" username: "test" password: "testtest")
+User.create(name: "Robo", username: "test", password: "testtest")
 
-user.create(name: "Robo2" username: "test2" password: "testtest")
+User.create(name: "Robo2", username: "test2", password: "testtest")
 
 (1..20).to_a.each do |num|
     Project.create(name: "Project. #{num}")
