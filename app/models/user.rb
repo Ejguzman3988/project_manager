54a151fb3d7ddbeb08163114819245fbea8dba60
ejguzman3988 <eriberto176@gmail.com>
2020-07-23 04:27:25 -0400

    Added limits to user's Username and Name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
     has_many :projects
     has_secure_password
 
-    validates :name, presence: true, uniqueness: true
-    validates :username, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true, length: {maximum: 20}
+    validates :username, presence: true, uniqueness: true, length: {maximum: 20}
+
 end
