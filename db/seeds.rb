02d686ff4319b2c4bffc5e5d1efb5ef1468d0a9f
ejguzman3988 <eriberto.guzman@outlook.com>
2021-03-26 23:23:33 -0400

    Added ability to seed data for notifications for testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,44 @@
+count = 0
+total = 100
+
 # 10.times do 
+#     system('clear')
+#     count +=1
+#     puts("#{count/(total.to_f)*100}%")
 #     User.create(
 #         name: Faker::Name.name, 
 #         username: Faker::Internet.username, 
 #         password: "testtest"
 #     )
 # end
+# puts "Created Random users"
 
+# puts "Creating Projects"
 
-puts "Creating Projects"
-count = 0
-total = 1000
+# total.times do
+#     system('clear')
+#     count +=1
+#     puts("#{count/(total.to_f)*100}%")
+    
+#     Project.create(
+#         name: Faker::App.name,
+#         description: Faker::Quote.yoda, 
+#         img_link: Faker::Avatar.image, 
+#         user_id: User.ids.sample
+#     )
+# end
+
+# puts "created projects"
+
 total.times do
     system('clear')
-    count +=1
+    count += 1
     puts("#{count/(total.to_f)*100}%")
-    
-    Project.create(
-        name: Faker::App.name,
-        description: Faker::Quote.yoda, 
-        img_link: Faker::Avatar.image, 
-        user_id: User.ids.sample
-    )
+
+    Notification.create( user_id: User.ids.sample, project_id: Project.ids.sample, join_request: [nil, true, false].sample)
+
 end
-puts "created projects"
+puts "Added random users to random projects and gave random notifications."
+
+
+
